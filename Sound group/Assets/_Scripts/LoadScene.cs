eae079de8d9b071ad73659b6a31d1f5328f3179d
OCGTenno <44306386+OCGTenno@users.noreply.github.com>
2018-12-01 12:50:33 -0500

    Tweaks to Collecting Letter Script so i can access it in the LoadScene, finished LoadScene (Scene manager), made a shell for the main menu script

---
+++ Sound group/Assets/_Scripts/LoadScene.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    public int letters = Collectingletters.collectedLetters;
+    public GameObject boxTrigger;
+
+
+    Scene scene;
+    private string Floor;
+
+    void Start()
+    {
+        boxTrigger.SetActive(false);   // Set the Trigger to off so they cant skip to the next level
+
+        scene = SceneManager.GetActiveScene();  // Check scene and assign names of the scenes to each floor
+
+
+    }
+
+    // Use this for initialization
+    public void onTriggerEnter () {
+
+        if (scene.name == "CliffBrandon Floor2")
+        {
+            if (letters == 3)
+            {
+                SceneManager.LoadScene("AlexShayne Floor1");
+            }
+        }
+
+        if (scene.name == "AlexShayne Floor1")
+        {
+            if (letters == 6)
+            {
+                SceneManager.LoadScene("Theater");
+            }
+        }
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (scene.name == "CliffBrandon Floor2")
+        {
+            if (letters == 3)
+            {
+                boxTrigger.SetActive(true);
+            }
+        }
+
+        else if (scene.name == "AlexShayne Floor1")
+        {
+            if (letters == 6)
+            {
+                boxTrigger.SetActive(true);
+            }
+        }
+
+        else
+        {
+            boxTrigger.SetActive(false);
+        }
+
+    }
+}
---
+++ Sound group/Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void StartGame () {
+        SceneManager.LoadScene("CliffBrandon Floor2");
+	}
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
