a83c898fca8ef5f7cf093496688693a3673687c8
OCGTenno <44306386+OCGTenno@users.noreply.github.com>
2018-12-12 16:15:09 -0500

    Modified Collecting Letters so the Scene changes upon collecting x amount of letters

--- Sound group/Assets/_Scripts/Collectingletters.cs
+++ Sound group/Assets/_Scripts/Collectingletters.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Collectingletters : MonoBehaviour
 {
+
+    Scene scene;
+
     public GameObject[]letters = new GameObject[7];
-    public static int collectedLetters;
+    public int collectedLetters;
     public GameObject Player;
     public AudioClip collectSound;
     
 	// Use this for initialization
 	void Start () {
-		
-	}
+        scene = SceneManager.GetActiveScene();
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -25,14 +29,29 @@ public class Collectingletters : MonoBehaviour
 
         for (int i = 0; i < letters.Length; i++)
         {
-
-
             if (collider.transform.gameObject.name == letters[i].name)
             {
                 collectedLetters++;
                 collider.gameObject.SetActive(false);
                 AudioSource.PlayClipAtPoint(collectSound, transform.position);
                 
+            }
+
+            if (scene.name == "CliffBrandon Floor2")
+            {
+                if (collectedLetters == 3 && collider.transform.gameObject.name == "Trigger")
+                {
+                    SceneManager.LoadScene("AlexShayne Floor1");
+                }
+            }
+
+            if (scene.name == "AlexShayne Floor1")
+            {
+                if (collectedLetters == 3 && collider.transform.gameObject.name == "Trigger")
+                {
+                    SceneManager.LoadScene("Theater");
+                    Debug.Log("Working");
+                }
             }
         }
     }
